ef012fb85a4b7f378c4dbd3669816a6643fb5447
menachem <ma0556703449@gmail.com>
2024-09-26 16:29:05 +0300

    fix the flask and end part 4

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from repository.db import create_tables, drop_tables
 from model import *
 
 app = Flask(__name__)
+app.register_blueprint(target_blueprint, url_prefix="/api/mission")
 
 if __name__ == '__main__':
     create_tables()
-    app.register_blueprint(target_blueprint, url_prefix="/api/mission")
     app.run(debug=True)
 
     # drop_tables()
